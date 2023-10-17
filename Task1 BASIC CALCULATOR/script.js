b704fc6b6c37000e0fcf48001f8797202b822e44
Tamal Barman <tamalbarman86@gmail.com>
2023-10-17 14:46:16 +0530

    adding task1

---
+++ Task1 BASIC CALCULATOR/script.js
@@ -0,0 +1,35 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      try {
+        string = eval(string);
+        if (string === Infinity) {
+          input.value = "Cannot divide by zero";
+          string = "";
+        } else {
+          input.value = string;
+        }
+      } catch (error) {
+        input.value = "Error";
+        string = "";
+      }
+    } else if (e.target.innerHTML === "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML === "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else if (e.target.innerHTML === "%") {
+      string = (parseFloat(string) / 100).toString();
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
